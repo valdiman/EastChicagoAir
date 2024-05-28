eda2f42ba4f4875a33629aa6b433506165fb735d
martie <andres-martinez@uiowa.edu>
2024-05-28 17:21:21 -0500

    still working on this.

--- R/LOQ.R
+++ R/LOQ.R
@@ -24,7 +24,9 @@ m.b <- subset(m, In.Out == "BLANK")
 # Select columns that start with "PCB"
 pcb_columns <- grep("^PCB", names(a.b), value = TRUE)
 pcb_a.b <- a.b[, pcb_columns]
+pcb_m.b <- m.b[, pcb_columns]
 
+# (1) a data
 # Apply Shapiro-Wilk test to each PCB column
 shapiro_results <- lapply(pcb_a.b, shapiro.test)
 # Extract the p-values from the results
@@ -35,7 +37,6 @@ p_values_normal <- p_values[p_values > 0.05]
 # 75% show normality via this test
 
 # Function to create Q-Q plots for each PCB column
-# Revised function to include the column name in the plot
 plot_qq <- function(column_data, column_name) {
   qqnorm(column_data, main = paste("Q-Q plot of", column_name))
   qqline(column_data, col = "steelblue", lwd = 2)
@@ -61,14 +62,10 @@ invisible(lapply(names(pcb_a.b), function(col_name) {
   plot_qq(log10(pcb_a.b[[col_name]]), col_name)
 }))
 
-# Better option to use log10 data to calculate LOQ
-# Create LOQ, i.e., upper 95 CI% (mean + 1.96*sd/(n)^0.5)
-log10pcb_a.b <- log10(pcb_a.b) # log10 blank data
-loq.a <- colMeans(log10pcb_a.b, na.rm = TRUE) + 1.96*sapply(log10pcb_a.b, sd, na.rm = TRUE)/sqrt(5)
-loq.a <- data.frame(t(loq.a))
-
-
+# (2) m data
 pcb_m.b <- m.b[, pcb_columns]
+# There are 2 zeros due to the different SS use here, PCB14 and PCB128+166
+pcb_m.b[pcb_m.b == 0] <- NA
 
 # Apply Shapiro-Wilk test to each PCB column
 shapiro_results <- lapply(pcb_m.b, shapiro.test)
@@ -106,11 +103,58 @@ invisible(lapply(names(pcb_m.b), function(col_name) {
   plot_qq(log10(pcb_m.b[[col_name]]), col_name)
 }))
 
+# Calculate LOQ -----------------------------------------------------------
 # Better option to use log10 data to calculate LOQ
 # Create LOQ, i.e., upper 95 CI% (mean + 1.96*sd/(n)^0.5)
+log10pcb_a.b <- log10(pcb_a.b) # log10 blank data
+loq.a <- colMeans(log10pcb_a.b,
+                  na.rm = TRUE) + 1.96*sapply(log10pcb_a.b,
+                                              sd, na.rm = TRUE)/sqrt(5)
+loq.a <- data.frame(t(loq.a))
+
+# Create LOQ, i.e., upper 95 CI% (mean + 1.96*sd/(n)^0.5)
 log10pcb_m.b <- log10(pcb_m.b) # log10 blank data
-loq.m <- colMeans(log10pcb_m.b, na.rm = TRUE) + 1.96*sapply(log10pcb_m.b, sd, na.rm = TRUE)/sqrt(9)
+loq.m <- colMeans(log10pcb_m.b,
+                  na.rm = TRUE) + 1.96*sapply(log10pcb_m.b,
+                                              sd, na.rm = TRUE)/sqrt(9)
 loq.m <- data.frame(t(loq.m))
+
+# Samples vs LOQ (masses) -------------------------------------------------
+# Select samples
+sample.a <- subset(a, In.Out %in% c("IN", "OUT"))
+# Select only PCBs
+sample.a <- sample.a[, pcb_columns]
+sample.a <- sapply(sample.a, as.numeric)
+sample.a[sample.a == 0] <- NA
+# Log10 transformation with handling of zeros
+log10_sample.a <- log10(sample.a)
+
+sample.m <- subset(m, In.Out %in% c("outdoor", "IN"))
+# Select only PCBs
+sample.m <- sample.m[, pcb_columns]
+sample.m <- sapply(sample.m, as.numeric)
+sample.m[sample.m == 0] <- NA
+# Log10 transformation with handling of zeros
+log10_sample.m <- log10(sample.m)
+
+# If samples > loq, then sample, if not 0
+# Create matrix to storage comparison
+a.2 <- matrix(NA, nrow = dim(sample.a)[1], ncol = dim(sample.a)[2])
+# Do comparison
+for(i in 1:dim(sample.a)[1]) {
+  for(j in 1:dim(sample.a)[2]) {
+    if (log10_sample.a[i, j] > loq.a[j]) {
+      a.2[i, j] <- sample.a[i, j]
+    } else {
+      a.2[i, j] <- 0
+    }
+  }
+}
+
+
+
+
+
 
 
 
