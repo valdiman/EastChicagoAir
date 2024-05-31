6cb37933e5e35762aa8f8cc62fcbfd7658ce182f
martie <andres-martinez@uiowa.edu>
2024-05-31 15:55:26 -0500

    initial version.

---
+++ R/MassSSCorrection.R
@@ -0,0 +1,7 @@
+# Mass SS correction
+
+# Read data ---------------------------------------------------------------
+a <- read.csv("Output/Data/csv/Sample_aV02.csv")
+m <- read.csv("Output/Data/csv/Sample_mV02.csv")
+
+a.t <- t(a)
