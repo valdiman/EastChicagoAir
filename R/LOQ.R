e2fca5e6020d143011f45f8fe02d0fd0fada50b5
martie <andres-martinez@uiowa.edu>
2024-05-31 13:45:14 -0500

    still working on this.

--- R/LOQ.R
+++ R/LOQ.R
@@ -16,21 +16,21 @@ m <- data.frame(read_xlsx("Data/RawDataRachel.xlsx", sheet = "group2Maeve",
                           col_names = TRUE, col_types = NULL))
 
 # Select blank data -------------------------------------------------------
-a.b <- subset(a, In.Out %in% c("method blank", "BLANK", "Blank, last cell"))
-m.b <- subset(m, In.Out == "BLANK")
+blank.a <- subset(a, In.Out %in% c("method blank", "BLANK", "Blank, last cell"))
+blank.m <- subset(m, In.Out == "BLANK")
 
 # Select columns that start with "PCB"
-pcb_columns <- grep("^PCB", names(a.b), value = TRUE)
-pcb_a.b <- a.b[, pcb_columns]
-pcb_m.b <- m.b[, pcb_columns]
+pcb_columns <- grep("^PCB", names(a), value = TRUE)
+blank.a <- blank.a[, pcb_columns]
+blank.m <- blank.m[, pcb_columns]
 
 # Check normality ---------------------------------------------------------
 # (1) a data
 # Apply Shapiro-Wilk test to each PCB column
-shapiro_results <- lapply(pcb_a.b, shapiro.test)
+shapiro_results <- lapply(blank.a, shapiro.test)
 # Extract the p-values from the results
 p_values <- sapply(shapiro_results, function(x) x$p.value)
-names(p_values) <- names(pcb_a.b)
+names(p_values) <- names(blank.a)
 # Select p-values greater than 0.05
 p_values_normal <- p_values[p_values > 0.05] # 75% show normality via this test
 
@@ -41,128 +41,195 @@ plot_qq <- function(column_data, column_name) {
 }
 
 # Apply the function to each PCB column with its name
-invisible(lapply(names(pcb_a.b), function(col_name) {
-  plot_qq(pcb_a.b[[col_name]], col_name)
+invisible(lapply(names(blank.a), function(col_name) {
+  plot_qq(blank.a[[col_name]], col_name)
 }))
 
 # Apply Shapiro-Wilk test to each PCB column log10
-shapiro_results_log10 <- lapply(log10(pcb_a.b), shapiro.test)
+shapiro_results_log10 <- lapply(log10(blank.a), shapiro.test)
 # Extract the p-values from the results
 p_values_log10 <- sapply(shapiro_results_log10, function(x) x$p.value)
-names(p_values_log10) <- names(pcb_a.b)
+names(p_values_log10) <- names(blank.a)
 # Select p-values greater than 0.05
 p_values_normal_log10 <- p_values_log10[p_values_log10 > 0.05] # 94% show normality via this test
 
 #Plot
 # Apply the function to each column
-invisible(lapply(names(pcb_a.b), function(col_name) {
-  plot_qq(log10(pcb_a.b[[col_name]]), col_name)
+invisible(lapply(names(blank.a), function(col_name) {
+  plot_qq(log10(blank.a[[col_name]]), col_name)
 }))
 
 # (2) m data
-pcb_m.b <- m.b[, pcb_columns]
 # There are 2 zeros due to the different SS use here, PCB14 and PCB128+166
-pcb_m.b[pcb_m.b == 0] <- NA
+blank.m[blank.m == 0] <- NA
 
 # Apply Shapiro-Wilk test to each PCB column
-shapiro_results <- lapply(pcb_m.b, shapiro.test)
+shapiro_results <- lapply(blank.m, shapiro.test)
 # Extract the p-values from the results
 p_values <- sapply(shapiro_results, function(x) x$p.value)
-names(p_values) <- names(pcb_m.b)
+names(p_values) <- names(blank.m)
 # Select p-values greater than 0.05
 p_values_normal <- p_values[p_values > 0.05] # 42% show normality via this test
 
-# Function to create Q-Q plots for each PCB column
-# Revised function to include the column name in the plot
-plot_qq <- function(column_data, column_name) {
-  qqnorm(column_data, main = paste("Q-Q plot of", column_name))
-  qqline(column_data, col = "steelblue", lwd = 2)
-}
-
 # Apply the function to each PCB column with its name
-invisible(lapply(names(pcb_m.b), function(col_name) {
-  plot_qq(pcb_m.b[[col_name]], col_name)
+invisible(lapply(names(blank.m), function(col_name) {
+  plot_qq(blank.m[[col_name]], col_name)
 }))
 
 # Apply Shapiro-Wilk test to each PCB column log10
-shapiro_results_log10 <- lapply(log10(pcb_m.b), shapiro.test)
+shapiro_results_log10 <- lapply(log10(blank.m), shapiro.test)
 # Extract the p-values from the results
 p_values_log10 <- sapply(shapiro_results_log10, function(x) x$p.value)
-names(p_values_log10) <- names(pcb_m.b)
+names(p_values_log10) <- names(blank.m)
 # Select p-values greater than 0.05
 p_values_normal_log10 <- p_values_log10[p_values_log10 > 0.05] # 96% show normality via this test
 
 #Plot
 # Apply the function to each column
-invisible(lapply(names(pcb_m.b), function(col_name) {
-  plot_qq(log10(pcb_m.b[[col_name]]), col_name)
+invisible(lapply(names(blank.m), function(col_name) {
+  plot_qq(log10(blank.m[[col_name]]), col_name)
 }))
 
 # Calculate LOQ -----------------------------------------------------------
 # Better option to use log10 data to calculate LOQ
 # Create LOQ for a, i.e., upper 95 CI% (mean + 1.96*sd/(n)^0.5)
-log10pcb_a.b <- log10(pcb_a.b) # log10 blank data
-loq.a <- colMeans(log10pcb_a.b,
-                  na.rm = TRUE) + 1.96*sapply(log10pcb_a.b,
-                                              sd, na.rm = TRUE)/sqrt(5)
+log10blank.a <- log10(blank.a) # log10 blank data
+loq.a <- colMeans(log10blank.a,
+                  na.rm = TRUE) + 1.96*sapply(log10blank.a,
+                                              sd, na.rm = TRUE)/sqrt(nrow(log10blank.a))
 loq.a <- data.frame(t(loq.a))
+# Check for Na and -Inf
+any(is.na(loq.a))
+any(loq.a == -Inf)
+# Convert loq.a to a numeric vector
+loq.a <- as.numeric(loq.a)
 
 # Create LOQ for m, i.e., upper 95 CI% (mean + 1.96*sd/(n)^0.5)
-log10pcb_m.b <- log10(pcb_m.b) # log10 blank data
-loq.m <- colMeans(log10pcb_m.b,
-                  na.rm = TRUE) + 1.96*sapply(log10pcb_m.b,
-                                              sd, na.rm = TRUE)/sqrt(9)
+log10blank.m <- log10(blank.m) # log10 blank data
+# Replace -Inf with NA for PCBs14 and 128+166
+log10blank.m[log10blank.m == -Inf] <- NA
+loq.m <- colMeans(log10blank.m,
+                  na.rm = TRUE) + 1.96*sapply(log10blank.m,
+                                              sd, na.rm = TRUE)/sqrt(nrow(log10blank.m))
 loq.m <- data.frame(t(loq.m))
+# Check for Na and -Inf
+any(is.na(loq.m))
+any(loq.m == -Inf)
+# Convert loq.a to a numeric vector
+loq.m <- as.numeric(loq.m)
 
 # Samples vs LOQ (masses) -------------------------------------------------
 # Select samples from a, subset only PCBs, and convert to numeric
+sample.a.0 <- subset(a, In.Out %in% c("IN", "OUT"))
 sample.a <- sapply(subset(a, In.Out %in% c("IN", "OUT"))[, pcb_columns],
                    as.numeric)
 
-# Log10 transformation excluding zeros
-log10_sample.a <- as.matrix(log10(sample.a))
-#log10_sample.a[log10_sample.a == -Inf] <- NA
+# Log10 transformation excluding zeros and replacing with NA
+log10sample.a <- as.matrix(log10(ifelse(sample.a == 0, NA, sample.a)))
+# Change NaN to NA
+log10sample.a[is.nan(log10sample.a)] <- NA
+any(is.nan(log10sample.a)) # False
+# Change -Inf to NA
+log10sample.a[log10sample.a == -Inf] <- NA
+any(log10sample.a == -Inf) # true
 
-# Perform the comparison and assignment using vectorized operations per column
-sample.a.2 <- matrix(0, nrow = nrow(log10_sample.a), ncol = ncol(log10_sample.a))
-colnames(sample.a.2) <- colnames(log10_sample.a)
-
-for (j in 1:ncol(sample.a.2)) {
-  sample.a.2[, j] <- ifelse(log10_sample.a[, j] > loq.a[j],
-                            10^log10_sample.a[, j], NA)
+# Perform the comparison a
+# (1) below loq -> 0
+sample.a.c <- matrix(NA, nrow = nrow(log10sample.a), ncol = ncol(log10sample.a))
+colnames(sample.a.c) <- colnames(log10sample.a)
+for (j in 1:ncol(sample.a.c)) {
+  for (i in 1:nrow(sample.a.c)) {
+    if (!is.na(log10sample.a[i, j])) {
+      if (log10sample.a[i, j] > loq.a[j]) {
+        sample.a.c[i, j] <- 10^log10sample.a[i, j]
+      } else {
+        sample.a.c[i, j] <- 0
+      }
+    } else {
+      sample.a.c[i, j] <- NA
+    }
+  }
 }
 
+# Add metadata
+sample.a.c <- cbind(sample.a.0[, c("In.Out", "Date.Placed",
+                                   "Date.Collected..shipment.date.for.FB", "SSOCB13C.3")],
+                    sample.a.c)
+                    
+                    $In.Out, sample.a.0$Date.Placed,
+                    sample.a.0$Date.Collected..shipment.date.for.FB.,
+                    sample.a.0$SSPCB13C.3,
+                    sample.a.c)
 
-# Here!
+# (2) below loq -> loq/(2)^0.5
+sample.a.c.2 <- matrix(NA, nrow = nrow(log10sample.a), ncol = ncol(log10sample.a))
+colnames(sample.a.c.2) <- colnames(log10sample.a)
+for (j in 1:ncol(sample.a.c.2)) {
+  for (i in 1:nrow(sample.a.c.2)) {
+    if (!is.na(log10sample.a[i, j])) {
+      if (log10sample.a[i, j] > loq.a[j]) {
+        sample.a.c.2[i, j] <- 10^log10sample.a[i, j]
+      } else {
+        sample.a.c.2[i, j] <- 10^(loq.a[j]/sqrt(2))
+      }
+    } else {
+      sample.a.c.2[i, j] <- NA
+    }
+  }
+}
 
 # Export results
-write.csv(sample.a.2, file = "Output/Data/csv/Sample_aV01.csv",
+write.csv(sample.a.c, file = "Output/Data/csv/Sample_aV01.csv",
           row.names = FALSE)
-
-# (ii) Perform the comparison, if below loq => loq/(2)^0.5
-a.3 <- ifelse(log10_sample.a > loq.a[[1]], log10_sample.a, loq.a[[1]]/sqrt(2))
-
-# Transform back using 10^, leaving zeros unchanged
-sample.a.3 <- ifelse(a.3 == 0, 0, 10^a.3)
-
-# Export results
-write.csv(sample.a.3, file = "Output/Data/csv/Sample_aV02.csv",
+write.csv(sample.a.c.2, file = "Output/Data/csv/Sample_aV02.csv",
           row.names = FALSE)
-
 
 # Select samples from m, subset only PCBs, and convert to numeric
 sample.m <- sapply(subset(m, In.Out %in% c("IN", "OUT"))[, pcb_columns],
                    as.numeric)
+# Log10 transformation excluding zeros and replacing with NA
+log10sample.m <- as.matrix(log10(ifelse(sample.m == 0, NA, sample.m)))
+# Change NaN to NA
+log10sample.m[is.nan(log10sample.m)] <- NA
+any(is.nan(log10sample.m)) # False
+# Change -Inf to NA
+log10sample.m[log10sample.m == -Inf] <- NA
+any(log10sample.m == -Inf) # true
 
-# Log10 transformation excluding zeros
-log10_sample.m <- log10(sample.m)
-log10_sample.m[sample.m == 0] <- 0
+# Perform the comparison m
+# (1) below loq -> 0
+sample.m.c <- matrix(NA, nrow = nrow(log10sample.m), ncol = ncol(log10sample.m))
+colnames(sample.m.c) <- colnames(log10sample.m)
+for (j in 1:ncol(sample.m.c)) {
+  for (i in 1:nrow(sample.m.c)) {
+    if (!is.na(log10sample.m[i, j])) {
+      if (log10sample.m[i, j] > loq.m[j]) {
+        sample.m.c[i, j] <- 10^log10sample.m[i, j]
+      } else {
+        sample.m.c[i, j] <- 0
+      }
+    } else {
+      sample.m.c[i, j] <- NA
+    }
+  }
+}
 
-# Perform the comparison and assignment using vectorized operations
-m.2 <- ifelse(log10_sample.m > loq.m[[1]], log10_sample.m, 0)
-
-# Transform back using 10^, leaving zeros unchanged
-sample.m.2 <- ifelse(m.2 == 0, 0, 10^m.2)
+# (2) below loq -> loq/(2)^0.5
+sample.m.c.2 <- matrix(NA, nrow = nrow(log10sample.m), ncol = ncol(log10sample.m))
+colnames(sample.m.c.2) <- colnames(log10sample.m)
+for (j in 1:ncol(sample.m.c.2)) {
+  for (i in 1:nrow(sample.m.c.2)) {
+    if (!is.na(log10sample.m[i, j])) {
+      if (log10sample.m[i, j] > loq.m[j]) {
+        sample.m.c.2[i, j] <- 10^log10sample.m[i, j]
+      } else {
+        sample.m.c.2[i, j] <- 10^(loq.m[j]/sqrt(2))
+      }
+    } else {
+      sample.m.c.2[i, j] <- NA
+    }
+  }
+}
 
 # Export results
 write.csv(sample.m.2, file = "Output/Data/csv/Sample_m.csv",
