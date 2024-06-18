98c63ee5807bddfefc88210508e2518a3d9bcbf0
martie <andres-martinez@uiowa.edu>
2024-06-18 14:46:42 -0500

    deteled it.

--- R/MassSSCorrection.R
+++
@@ -1,7 +0,0 @@
-# Mass SS correction
-
-# Read data ---------------------------------------------------------------
-a <- read.csv("Output/Data/csv/Sample_aV02.csv")
-m <- read.csv("Output/Data/csv/Sample_mV02.csv")
-
-a.t <- t(a)
