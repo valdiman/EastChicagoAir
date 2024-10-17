3e5442498ecfd22d7daa4af2f28c4d66f9b29ce6
Andres Martinez <34082961+valdiman@users.noreply.github.com>
2024-10-17 11:02:53 -0500

    Update Subfolders.R

--- Subfolders.R
+++ Subfolders.R
@@ -2,7 +2,10 @@
 
 dir.create("R")
 dir.create("Data")
+dir.create("Data/PUFPASModel")
 dir.create("Output")
 dir.create("Output/Data")
 dir.create("Output/Data/csv")
 dir.create("Output/Plots")
+dir.create("Output/Data/PUFPASModel/isd_light")
+dir.create("Output/Data/PUFPASModel/Results")
