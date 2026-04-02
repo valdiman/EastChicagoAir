46fe4bbd9884ef90db091e107395b92efe236d3e
martie <andres-martinez@uiowa.edu>
2026-04-02 18:26:57 -0500

    updated version.

--- Subfolders.R
+++ Subfolders.R
@@ -5,13 +5,19 @@ dir.create("R/Analysis")
 dir.create("R/ConcentrationCalculations")
 dir.create("R/PUFPASModel")
 dir.create("R/Flux")
+dir.create("R/Soil")
+dir.create("R/Soil/Kriging")
+dir.create("R/Soil/ReadDataPangaea")
+
 dir.create("Data")
 dir.create("Data/PUFPASModel")
 dir.create("Data/Flux")
+dir.create("Data/Soil")
+
 dir.create("Output")
 dir.create("Output/Data")
-dir.create("Output/Data/csv")
-dir.create("Output/Data/excel")
+dir.create("Output/Data/Soil")
+
 dir.create("Output/PUFPASModel")
 dir.create("Output/PUFPASModel/Meteo")
 dir.create("Output/PUFPASModel/Meteo/isd_light")
@@ -20,6 +26,9 @@ dir.create("Output/PUFPASModel/Vef")
 dir.create("Output/PUFPASModel/Vef/isd_light")
 dir.create("Output/PUFPASModel/Vef/MERRA")
 dir.create("Output/Plots")
+
+dir.create("Output/Soil")
+
 dir.create("Output/Plots/Concentrations")
 
 
