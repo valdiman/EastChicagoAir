28f8e4c8b9c53e1eb71461ca69d52ad62a57a14c
Andres Martinez <martie@uiowa.edu>
2026-04-16 14:32:46 -0500

    updated version.

--- Subfolders.R
+++ Subfolders.R
@@ -1,29 +1,34 @@
 # Code to create sub folders
 
 dir.create("R")
-dir.create("R/AirConcentrationCalculations")
-dir.create("R/PUFPASModel")
-dir.create("R/Flux")
-dir.create("R/SoilAirExchange")
-dir.create("R/Soil")
-dir.create("R/DataAcquisitionNOAA")
 dir.create("R/AirChicagoData")
+dir.create("R/AirConcCalculations")
+dir.create("R/AirConcCalculations/ACE")
+dir.create("R/AirConcCalculations/AESOP")
+dir.create("R/AirConcCalculations/AESOP/PUFPASMOdel")
+dir.create("R/DataAcquisitionNOAA")
+dir.create("R/Extra")
+dir.create("R/Extra/Flux")
+dir.create("R/Extra/Soil")
+dir.create("R/Extra/SoilAirExchange")
 
 dir.create("Data")
 dir.create("Data/Air")
 dir.create("Data/Air/Chicago")
 dir.create("Data/Air/EastChicago")
-dir.create("Data/PUFPASModel")
-dir.create("Data/Flux")
-dir.create("Data/Soil")
-dir.create("Data/PCProperties")
+dir.create("Data/Air/EastChicago/ACE")
+dir.create("Data/Air/EastChicago/AESOP")
+dir.create("Data/Air/EastChicago/AESOP/PUFPASModel")
+dir.create("Data/Extra")
+dir.create("Data/Extra/Flux")
+dir.create("Data/Extra/Soil")
 dir.create("Data/Meteorology")
+dir.create("Data/PCProperties")
 
 dir.create("Output")
 dir.create("Output/Data")
 dir.create("Output/Data/Air")
 dir.create("Output/Data/Soil")
-
 dir.create("Output/PUFPASModel")
 dir.create("Output/PUFPASModel/Meteo")
 dir.create("Output/PUFPASModel/Meteo/isd_light")
@@ -32,9 +37,9 @@ dir.create("Output/PUFPASModel/Vef")
 dir.create("Output/PUFPASModel/Vef/isd_light")
 dir.create("Output/PUFPASModel/Vef/MERRA")
 dir.create("Output/Plots")
-
+dir.create("Output/Plots/Concentrations")
 dir.create("Output/Soil")
 
-dir.create("Output/Plots/Concentrations")
+
 
 
