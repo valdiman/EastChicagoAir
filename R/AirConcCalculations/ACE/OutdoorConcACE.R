0cb2b7f2313aa97f0af6788ac9bdc61b8bb8c635
Andres Martinez <martie@uiowa.edu>
2026-05-29 08:57:47 -0500

    Changed Date to date from the activity_daily file.

--- R/AirConcCalculations/ACE/OutdoorConcACE.R
+++ R/AirConcCalculations/ACE/OutdoorConcACE.R
@@ -66,7 +66,7 @@ activities_clean <- act.data %>%
 
 # Create full daily timeline
 all_dates <- data.frame(
-  Date = seq(min(activities_clean$DateStart),
+  date = seq(min(activities_clean$DateStart),
              max(activities_clean$DateEnd),
              by = "day")
 )
@@ -85,7 +85,7 @@ get_activity <- function(d, df) {
 }
 
 activity_daily <- all_dates %>%
-  mutate(Activity = map_chr(Date, get_activity, df = activities_clean))
+  mutate(Activity = map_chr(date, get_activity, df = activities_clean))
 
 # Export data
 write.csv(activity_daily, "Data/RemediationProject/activity_daily.csv",
@@ -95,7 +95,7 @@ write.csv(activity_daily, "Data/RemediationProject/activity_daily.csv",
 # PCB 8
 p.pcb8 <- ggplot(ace, aes(x = date, y = PCB8)) +
   geom_tile(data = activity_daily,
-            aes(x = Date, y = 0, fill = Activity),
+            aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   geom_point(aes(color = location2, shape = PCB8_unc_label),
              size = 2) +
@@ -128,7 +128,7 @@ ggsave("Output/Plots/Concentrations/AcePCB8_CDF_HS.png", plot = p.pcb8,
 # PCB 15
 p.pcb15 <- ggplot(ace, aes(x = date, y = PCB15)) +
   geom_tile(data = activity_daily,
-            aes(x = Date, y = 0, fill = Activity),
+            aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   geom_point(aes(color = location2, shape = PCB15_unc_label),
              size = 2) +
@@ -162,7 +162,7 @@ ggsave("Output/Plots/Concentrations/AcePCB15_CDF_HS.png", plot = p.pcb15, width 
 # PCB 18+30
 p.pcb18 <- ggplot(ace, aes(x = date, y = PCB18.30)) +
   geom_tile(data = activity_daily,
-            aes(x = Date, y = 0, fill = Activity),
+            aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   geom_point(aes(color = location2, shape = PCB18.30_unc_label),
              size = 2) +
@@ -196,7 +196,7 @@ ggsave("Output/Plots/Concentrations/AcePCB18_CDF_HS.png", plot = p.pcb18,
 # PCB 20+28
 p.pcb20 <- ggplot(ace, aes(x = date, y = PCB20.28)) +
   geom_tile(data = activity_daily,
-            aes(x = Date, y = 0, fill = Activity),
+            aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   geom_point(aes(color = location2, shape = PCB20.28_unc_label),
              size = 2) +
@@ -230,7 +230,7 @@ ggsave("Output/Plots/Concentrations/AcePCB20_CDF_HS.png", plot = p.pcb20,
 # PCB 31
 p.pcb31 <- ggplot(ace, aes(x = date, y = PCB31)) +
   geom_tile(data = activity_daily,
-            aes(x = Date, y = 0, fill = Activity),
+            aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   geom_point(aes(color = location2, shape = PCB31_unc_label),
              size = 2) +
@@ -266,7 +266,7 @@ ggsave("Output/Plots/Concentrations/AcePCB31_CDF_HS.png", plot = p.pcb31,
 #PCB 8
 p.pcb8 <- ggplot(subset(ace, location2 == "South"),
                  aes(x = date, y = PCB8)) +
-  geom_tile(data = activity_daily, aes(x = Date, y = 0, fill = Activity),
+  geom_tile(data = activity_daily, aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   scale_fill_manual(name = "Project phase",
                     values = c("Dredging" = "#F4A6B7",
@@ -301,7 +301,7 @@ ggsave("Output/Plots/Concentrations/AcePCB8_CDF.png", plot = p.pcb8, width = 12,
 # PCB 15
 p.pcb15 <- ggplot(subset(ace, location2 == "South"),
                   aes(x = date, y = PCB15)) +
-  geom_tile(data = activity_daily, aes(x = Date, y = 0, fill = Activity),
+  geom_tile(data = activity_daily, aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   scale_fill_manual(name = "Project phase",
                     values = c("Dredging" = "#F4A6B7",
@@ -336,7 +336,7 @@ ggsave("Output/Plots/Concentrations/AcePCB15_CDF.png", plot = p.pcb15, width = 1
 # PCB 18+30
 p.pcb18 <- ggplot(subset(ace, location2 == "South"),
                   aes(x = date, y = PCB18.30)) +
-  geom_tile(data = activity_daily, aes(x = Date, y = 0, fill = Activity),
+  geom_tile(data = activity_daily, aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   scale_fill_manual(name = "Project phase",
                     values = c("Dredging" = "#F4A6B7",
@@ -371,7 +371,7 @@ ggsave("Output/Plots/Concentrations/AcePCB18_CDF.png", plot = p.pcb18, width = 1
 # PCB 20+28
 p.pcb20 <- ggplot(subset(ace, location2 == "South"),
                   aes(x = date, y = PCB20.28)) +
-  geom_tile(data = activity_daily, aes(x = Date, y = 0, fill = Activity),
+  geom_tile(data = activity_daily, aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   scale_fill_manual(name = "Project phase",
                     values = c("Dredging" = "#F4A6B7",
@@ -406,7 +406,7 @@ ggsave("Output/Plots/Concentrations/AcePCB20_CDF.png", plot = p.pcb20, width = 1
 # PCB 31
 p.pcb31 <- ggplot(subset(ace, location2 == "South"),
                   aes(x = date, y = PCB31)) +
-  geom_tile(data = activity_daily, aes(x = Date, y = 0, fill = Activity),
+  geom_tile(data = activity_daily, aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   scale_fill_manual(name = "Project phase",
                     values = c("Dredging" = "#F4A6B7",
@@ -441,7 +441,7 @@ ggsave("Output/Plots/Concentrations/AcePCB31_CDF.png", plot = p.pcb31, width = 1
 # HS
 p.pcb8 <- ggplot(subset(ace, location2 == "HS"),
                  aes(x = date, y = PCB8)) +
-  geom_tile(data = activity_daily, aes(x = Date, y = 0, fill = Activity),
+  geom_tile(data = activity_daily, aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   scale_fill_manual(name = "Project phase",
                     values = c("Dredging" = "#F4A6B7",
@@ -475,7 +475,7 @@ ggsave("Output/Plots/Concentrations/AcePCB8_HS.png", plot = p.pcb8, width = 12,
 # PCB 15
 p.pcb15 <- ggplot(subset(ace, location2 == "HS"),
                   aes(x = date, y = PCB15)) +
-  geom_tile(data = activity_daily, aes(x = Date, y = 0, fill = Activity),
+  geom_tile(data = activity_daily, aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   scale_fill_manual(name = "Project phase",
                     values = c("Dredging" = "#F4A6B7",
@@ -509,7 +509,7 @@ ggsave("Output/Plots/Concentrations/AcePCB15_HS.png", plot = p.pcb15, width = 12
 # PCB 18
 p.pcb18 <- ggplot(subset(ace, location2 == "HS"),
                   aes(x = date, y = PCB18.30)) +
-  geom_tile(data = activity_daily, aes(x = Date, y = 0, fill = Activity),
+  geom_tile(data = activity_daily, aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   scale_fill_manual(name = "Project phase",
                     values = c("Dredging" = "#F4A6B7",
@@ -543,7 +543,7 @@ ggsave("Output/Plots/Concentrations/AcePCB18_HS.png", plot = p.pcb18, width = 12
 # PCB20.28
 p.pcb20 <- ggplot(subset(ace, location2 == "HS"),
                   aes(x = date, y = PCB20.28)) +
-  geom_tile(data = activity_daily, aes(x = Date, y = 0, fill = Activity),
+  geom_tile(data = activity_daily, aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   scale_fill_manual(name = "Project phase",
                     values = c("Dredging" = "#F4A6B7",
@@ -577,7 +577,7 @@ ggsave("Output/Plots/Concentrations/AcePCB20_HS.png", plot = p.pcb20, width = 12
 # PCB 31
 p.pcb31 <- ggplot(subset(ace, location2 == "HS"),
                   aes(x = date, y = PCB31)) +
-  geom_tile(data = activity_daily, aes(x = Date, y = 0, fill = Activity),
+  geom_tile(data = activity_daily, aes(x = date, y = 0, fill = Activity),
             height = Inf, inherit.aes = FALSE, alpha = 0.3) +
   scale_fill_manual(name = "Project phase",
                     values = c("Dredging" = "#F4A6B7",
